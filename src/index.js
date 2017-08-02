e1692478a062022b1fd21a4a1c7783b4df026dfc
Jesse Wolgamott <jesse@comalproductions.com>
2017-08-02 13:20:35 -0500

    build multiple actions

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 
-// handleActions is our "reducer"
-// it it what gets called when an action happens
-// tldr: actions happen. Reducers decide what to do.
-// reducers set the state
-function handleActions(state=0, action){
+function numberReducer(state=0, action){
   if (action.type === 'INCREMENT'){
     state += 1
+  } else if (action.type === 'DECREMENT'){
+    state -= 1
+  } else if (action.type === 'SET'){
+    state = action.newValue
+  }
+  return state
+}
+function lastAction(state, action){
+  if (!state){
+    return new Date();
   }
   return state
 }
 
 // We need to have a store.
 // A store _stores_ our state
-const store = createStore(handleActions)
+const reducer = combineReducers({
+  number: numberReducer,
+  lastAction
+})
+const store = createStore(reducer)
 
 // Optional: what should happen when we update our state
 store.subscribe( () => {
   const state = store.getState()
   console.log("state", state)
+
+  document.querySelector("#important").textContent = state.number
+
 })
 
 // let's dispatch some actions
-store.dispatch({ type: 'INCREMENT'})
-store.dispatch({ type: 'INCREMENT'})
-store.dispatch({ type: 'INCREMENT'})
+setTimeout(() => store.dispatch({ type: 'SET', newValue: 5}), 2000)
+setTimeout(() => store.dispatch({ type: 'INCREMENT'}), 4000)
+setTimeout(() => store.dispatch({ type: 'DECREMENT'}), 6000)
+setTimeout(() => store.dispatch({ type: 'INCREMENT'}), 8000)
